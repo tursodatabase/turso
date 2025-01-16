e5a5883e3a146d52e4366d4d43c9e0ac27e875b8
seridescent <seridescent@gmail.com>
2025-01-16 14:41:50 -0500

    add python 3.9 to dev shell
    without it, `cargo test` fails with the following error when the python3 framework isn't available
    ```
    dyld[58788]: Library not loaded: @rpath/Python3.framework/Versions/3.9/Python3
      Referenced from: <5CA24EEF-B406-3A8A-B845-2C4A3737ED55> /Users/nicholast/projects/limbo/target/debug/deps/_limbo-89f6cdf4f3551f5a
      Reason: tried: '/Library/Frameworks/Python3.framework/Versions/3.9/Python3' (no such file), '/System/Library/Frameworks/Python3.framework/Versions/3.9/Python3' (no such file, not in dyld cache)
    error: test failed, to rerun pass `-p py-limbo --lib`
    ```
    
    admittedly, this might not be the minimal solution

--- flake.nix
+++ flake.nix
@@ -9,40 +9,45 @@
     };
   };
 
-  outputs = {
-    nixpkgs,
-    fenix,
-    ...
-  }: let
-    systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
-    forAllSystems = nixpkgs.lib.genAttrs systems;
-  in {
-    devShells = forAllSystems (
-      system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-        rustStable = fenix.packages.${system}.stable.toolchain;
-        wasmTarget = fenix.packages.${system}.targets.wasm32-wasi.latest.rust-std;
-        extraDarwinInputs =
-          if pkgs.stdenv.isDarwin
-          then [pkgs.darwin.apple_sdk.frameworks.CoreFoundation]
-          else [];
-      in {
-        default = with pkgs;
-          mkShell {
-            buildInputs =
-              [
-                clang
-                libiconv
-                sqlite
-                gnumake
-                rustup # not used to install the toolchain, but the makefile uses it
-                rustStable
-                wasmTarget
-                tcl
-              ]
-              ++ extraDarwinInputs;
-          };
-      }
-    );
-  };
+  outputs =
+    { nixpkgs
+    , fenix
+    , ...
+    }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          rustStable = fenix.packages.${system}.stable.toolchain;
+          wasmTarget = fenix.packages.${system}.targets.wasm32-wasi.latest.rust-std;
+          extraDarwinInputs =
+            if pkgs.stdenv.isDarwin
+            then [ pkgs.darwin.apple_sdk.frameworks.CoreFoundation ]
+            else [ ];
+        in
+        {
+          default = with pkgs;
+            mkShell {
+              buildInputs =
+                [
+                  clang
+                  libiconv
+                  sqlite
+                  gnumake
+                  rustup # not used to install the toolchain, but the makefile uses it
+                  rustStable
+                  wasmTarget
+                  tcl
+                  python39
+                ]
+                ++ extraDarwinInputs;
+            };
+        }
+      );
+    };
 }
