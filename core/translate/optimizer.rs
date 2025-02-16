e41f5fb51bac2a36eb0380ff14bc21184c414be7
Jussi Saurio <jussi.saurio@gmail.com>
2025-02-16 19:14:57 +0200

    Fix never evaluating constant conditions at all

--- core/translate/optimizer.rs
+++ core/translate/optimizer.rs
@@ -8,8 +8,8 @@ use crate::{
 };
 
 use super::plan::{
-    DeletePlan, Direction, IterationDirection, Operation, Plan, Search, SelectPlan, TableReference,
-    WhereTerm,
+    DeletePlan, Direction, EvalAt, IterationDirection, Operation, Plan, Search, SelectPlan,
+    TableReference, WhereTerm,
 };
 
 pub fn optimize_plan(plan: &mut Plan, schema: &Schema) -> Result<()> {
@@ -499,7 +499,7 @@ pub fn try_extract_index_search_expression(
     table_reference: &TableReference,
     available_indexes: &HashMap<String, Vec<Rc<Index>>>,
 ) -> Result<Option<Search>> {
-    if cond.eval_at_loop != table_index {
+    if !cond.should_eval_at_loop(table_index) {
         return Ok(None);
     }
     match &mut cond.expr {
@@ -512,7 +512,7 @@ pub fn try_extract_index_search_expression(
                             cmp_expr: WhereTerm {
                                 expr: rhs_owned,
                                 from_outer_join: cond.from_outer_join,
-                                eval_at_loop: cond.eval_at_loop,
+                                eval_at: EvalAt::Loop(table_index),
                             },
                         }));
                     }
@@ -526,7 +526,7 @@ pub fn try_extract_index_search_expression(
                             cmp_expr: WhereTerm {
                                 expr: rhs_owned,
                                 from_outer_join: cond.from_outer_join,
-                                eval_at_loop: cond.eval_at_loop,
+                                eval_at: EvalAt::Loop(table_index),
                             },
                         }));
                     }
@@ -542,7 +542,7 @@ pub fn try_extract_index_search_expression(
                             cmp_expr: WhereTerm {
                                 expr: lhs_owned,
                                 from_outer_join: cond.from_outer_join,
-                                eval_at_loop: cond.eval_at_loop,
+                                eval_at: cond.eval_at,
                             },
                         }));
                     }
@@ -556,7 +556,7 @@ pub fn try_extract_index_search_expression(
                             cmp_expr: WhereTerm {
                                 expr: lhs_owned,
                                 from_outer_join: cond.from_outer_join,
-                                eval_at_loop: cond.eval_at_loop,
+                                eval_at: cond.eval_at,
                             },
                         }));
                     }
@@ -580,7 +580,7 @@ pub fn try_extract_index_search_expression(
                             cmp_expr: WhereTerm {
                                 expr: rhs_owned,
                                 from_outer_join: cond.from_outer_join,
-                                eval_at_loop: cond.eval_at_loop,
+                                eval_at: cond.eval_at,
                             },
                         }));
                     }
@@ -604,7 +604,7 @@ pub fn try_extract_index_search_expression(
                             cmp_expr: WhereTerm {
                                 expr: lhs_owned,
                                 from_outer_join: cond.from_outer_join,
-                                eval_at_loop: cond.eval_at_loop,
+                                eval_at: cond.eval_at,
                             },
                         }));
                     }
