9dca09d9a4cd756acd0152120698e28eb57f4436
Ohkubo KOHEI <o@kbn.one>
2025-02-20 09:57:48 +0000

    fix format

--- bindings/ruby/src/lib.rs
+++ bindings/ruby/src/lib.rs
@@ -4,7 +4,7 @@ fn distance(a: (f64, f64), b: (f64, f64)) -> f64 {
     ((b.0 - a.0).powi(2) + (b.1 - a.1).powi(2)).sqrt()
 }
 
-#[magnus::init(name="limbo")]
+#[magnus::init(name = "limbo")]
 fn init(ruby: &Ruby) -> Result<(), Error> {
     ruby.define_global_function("distance", function!(distance, 2));
     Ok(())
