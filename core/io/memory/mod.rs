a764ff99028233c198961e3f2cd1ac23bf824c92
pedrocarlo <pedro.muniz.carlo@gmail.com>
2025-03-12 01:27:37 -0300

    modify memory/mod.rs to use the different implementations

--- core/io/memory/mod.rs
+++ core/io/memory/mod.rs
@@ -12,41 +12,130 @@ use btree::MemoryPages;
 
 use super::{Buffer, Completion, File, OpenFlags, IO};
 use crate::Result;
-
 use std::{
-    cell::{Cell, RefCell, UnsafeCell},
-    collections::BTreeMap,
+    cell::{Cell, OnceCell, RefCell, UnsafeCell},
     sync::Arc,
 };
+
 use tracing::debug;
 
+#[derive(Default)]
 pub struct MemoryIO {}
 unsafe impl Send for MemoryIO {}
+unsafe impl Sync for MemoryIO {}
 
 // TODO: page size flag
 const PAGE_SIZE: usize = 4096;
-type MemPage = Box<[u8; PAGE_SIZE]>;
+#[cfg(target_family = "unix")]
+pub type MemPage = [u8];
+
+#[cfg(not(target_family = "unix"))]
+pub type MemPage = Box<[u8; PAGE_SIZE]>;
+
+const INITIAL_PAGES: OnceCell<usize> = OnceCell::new();
+const DEFAULT_INITIAL_PAGES: usize = 16;
 
 impl MemoryIO {
-    #[allow(clippy::arc_with_non_send_sync)]
     pub fn new() -> Self {
-        debug!("Using IO backend 'memory'");
         Self {}
     }
 }
 
-impl Default for MemoryIO {
-    fn default() -> Self {
-        Self::new()
+impl MemoryFile {
+    #[allow(clippy::arc_with_non_send_sync)]
+    pub fn new() -> Result<Arc<Self>> {
+        debug!("Using IO backend 'memory'");
+        let pages = INITIAL_PAGES;
+        let initial_pages = *pages.get_or_init(|| {
+            std::env::var("INITIAL_MEM_PAGES").map_or(DEFAULT_INITIAL_PAGES, |str_num| {
+                let initial_pages = str_num.parse::<usize>().unwrap_or(DEFAULT_INITIAL_PAGES);
+                if initial_pages == 0 {
+                    panic!("INITIAL_MEM_PAGES flag cannot be zero")
+                }
+                initial_pages
+            })
+        });
+        Ok(Arc::new(Self {
+            pages: MemoryPages::new(PAGE_SIZE * initial_pages)?.into(),
+            size: 0.into(),
+        }))
+    }
+
+    #[allow(clippy::mut_from_ref)]
+    fn get_or_allocate_page(&self, page_no: usize) -> Result<&mut MemPage> {
+        #[cfg(target_family = "unix")]
+        {
+            let start = page_no * PAGE_SIZE;
+            let end = start + PAGE_SIZE;
+            let len_pages = unsafe {
+                let page = &mut *self.pages.get();
+                page.len()
+            };
+
+            if end > len_pages {
+                let cap_pages = len_pages / PAGE_SIZE;
+                self.resize(cap_pages * 2)?;
+            }
+
+            let page = unsafe {
+                let page = &mut *self.pages.get();
+                &mut page[start..end]
+            };
+            Ok(page)
+        }
+        #[cfg(not(target_family = "unix"))]
+        {
+            unsafe {
+                let pages = &mut *self.pages.get();
+                Ok(pages
+                    .entry(page_no)
+                    .or_insert_with(|| Box::new([0; PAGE_SIZE])))
+            }
+        }
+    }
+
+    fn get_page(&self, page_no: usize) -> Option<&MemPage> {
+        #[cfg(target_family = "unix")]
+        {
+            let start = page_no * PAGE_SIZE;
+            let end = start + PAGE_SIZE;
+            let pages = unsafe { &mut *self.pages.get() };
+
+            // Make sure that we are accessing into a valid memory page
+            assert!(end <= pages.len());
+
+            pages.get(start..end)
+        }
+        #[cfg(not(target_family = "unix"))]
+        unsafe {
+            (*self.pages.get()).get(&page_no)
+        }
+    }
+
+    #[cfg(target_family = "unix")]
+    fn resize(&self, capacity: usize) -> Result<()> {
+        let pages = unsafe { &mut *self.pages.get() };
+        let new_cap = PAGE_SIZE * capacity;
+        #[cfg(target_os = "linux")]
+        {
+            pages.remap(new_cap)?;
+        }
+
+        #[cfg(not(target_os = "linux"))]
+        {
+            let mut new_pages = MemoryPages::new(new_cap)?;
+            new_pages[..pages.len()].clone_from_slice(pages);
+
+            *pages = new_pages;
+        }
+
+        Ok(())
     }
 }
 
 impl IO for MemoryIO {
     fn open_file(&self, _path: &str, _flags: OpenFlags, _direct: bool) -> Result<Arc<dyn File>> {
-        Ok(Arc::new(MemoryFile {
-            pages: BTreeMap::new().into(),
-            size: 0.into(),
-        }))
+        MemoryFile::new().map(|f| f as Arc<dyn File>)
     }
 
     fn run_once(&self) -> Result<()> {
@@ -66,9 +155,10 @@ impl IO for MemoryIO {
 }
 
 pub struct MemoryFile {
-    pages: UnsafeCell<BTreeMap<usize, MemPage>>,
+    pages: UnsafeCell<MemoryPages>,
     size: Cell<usize>,
 }
+
 unsafe impl Send for MemoryFile {}
 unsafe impl Sync for MemoryFile {}
 
@@ -111,7 +201,6 @@ impl File for MemoryFile {
                 } else {
                     read_buf.as_mut_slice()[buf_offset..buf_offset + bytes_to_read].fill(0);
                 }
-
                 offset += bytes_to_read;
                 buf_offset += bytes_to_read;
                 remaining -= bytes_to_read;
@@ -140,7 +229,7 @@ impl File for MemoryFile {
             let bytes_to_write = remaining.min(PAGE_SIZE - page_offset);
 
             {
-                let page = self.get_or_allocate_page(page_no);
+                let page = self.get_or_allocate_page(page_no)?;
                 page[page_offset..page_offset + bytes_to_write]
                     .copy_from_slice(&data[buf_offset..buf_offset + bytes_to_write]);
             }
@@ -171,21 +260,9 @@ impl File for MemoryFile {
 impl Drop for MemoryFile {
     fn drop(&mut self) {
         // no-op
-    }
-}
-
-impl MemoryFile {
-    #[allow(clippy::mut_from_ref)]
-    fn get_or_allocate_page(&self, page_no: usize) -> &mut MemPage {
-        unsafe {
-            let pages = &mut *self.pages.get();
-            pages
-                .entry(page_no)
-                .or_insert_with(|| Box::new([0; PAGE_SIZE]))
-        }
-    }
-
-    fn get_page(&self, page_no: usize) -> Option<&MemPage> {
-        unsafe { (*self.pages.get()).get(&page_no) }
+        // TODO ideally we could have some flags
+        // in Memory File, that when this is dropped
+        // if the persist flag is true we could flush the mmap to disk
+        // We would also have to store the file name here in this case
     }
 }
