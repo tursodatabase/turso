0e93333c301eae8bec7bb6c34bb027631113a6e0
PThorpe92 <preston@unlockedlabs.org>
2025-03-15 13:16:02 -0400

    Add in-memory async vfs module in test extension

--- extensions/tests/src/lib.rs
+++ extensions/tests/src/lib.rs
@@ -8,12 +8,13 @@ use limbo_ext::{VfsDerive, VfsExtension, VfsFile};
 use std::collections::BTreeMap;
 use std::fs::{File, OpenOptions};
 use std::io::{Read, Seek, SeekFrom, Write};
-use std::sync::Mutex;
+use std::sync::atomic::AtomicUsize;
+use std::sync::{LazyLock, Mutex};
 
 register_extension! {
     vtabs: { KVStoreVTab },
     scalars: { test_scalar },
-    vfs: { TestFS },
+    vfs: { TestFS, AsyncTestVFS },
 }
 
 lazy_static! {
@@ -190,7 +191,7 @@ impl VfsExtension for TestFS {
 
 #[cfg(not(target_family = "wasm"))]
 impl VfsFile for TestFile {
-    fn read<F: FnOnce(i32)>(&mut self, mut buf: BufferRef, offset: i64, cb: F) {
+    fn read(&mut self, mut buf: BufferRef, offset: i64, cb: Box<dyn FnOnce(i32) + Send>) {
         log::debug!("reading file with testing VFS: offset: {offset}");
         if self.file.seek(SeekFrom::Start(offset as u64)).is_err() {
             cb(-1);
@@ -205,7 +206,7 @@ impl VfsFile for TestFile {
         cb(res);
     }
 
-    fn write<F: FnOnce(i32)>(&mut self, buf: BufferRef, offset: i64, cb: F) {
+    fn write(&mut self, buf: BufferRef, offset: i64, cb: Box<dyn FnOnce(i32) + Send>) {
         log::debug!(
             "writing to file with testing VFS: {} offset: {offset}",
             buf.len()
@@ -223,7 +224,7 @@ impl VfsFile for TestFile {
         cb(res);
     }
 
-    fn sync<F: FnOnce()>(&self, cb: F) {
+    fn sync(&self, cb: Box<dyn FnOnce() + Send>) {
         log::debug!("syncing file with testing VFS");
         let _ = self.file.sync_all();
         cb();
@@ -231,5 +232,166 @@ impl VfsFile for TestFile {
 
     fn size(&self) -> i64 {
         self.file.metadata().map(|m| m.len() as i64).unwrap_or(-1)
+    }
+}
+
+use std::collections::VecDeque;
+use std::sync::Arc;
+use tokio::{runtime::Runtime, task::JoinHandle, time::Duration};
+
+lazy_static! {
+    static ref RUNTIME: LazyLock<Runtime> = LazyLock::new(|| {
+        tokio::runtime::Builder::new_current_thread()
+            .enable_all()
+            .build()
+            .unwrap()
+    });
+    static ref TASK_QUEUE: LazyLock<Mutex<TaskQueue>> =
+        LazyLock::new(|| { Mutex::new(TaskQueue::new()) });
+}
+
+#[cfg(target_family = "wasm")]
+pub struct AsyncTestVFS;
+
+const PAGE_SIZE: usize = 4096;
+
+// This is just essentially the MemoryIO in core/io/memory.rs
+// with tokio + 'sleep's added to simulate async network IO
+// to ensure that we can support something like a S3 backend
+#[cfg(not(target_family = "wasm"))]
+#[derive(Default, VfsDerive)]
+pub struct AsyncTestVFS;
+
+pub struct AsyncTestFile {
+    storage: Arc<Mutex<BTreeMap<usize, Box<[u8; 4096]>>>>,
+    size: Arc<AtomicUsize>,
+}
+
+struct TaskQueue {
+    tasks: VecDeque<JoinHandle<()>>,
+}
+
+impl TaskQueue {
+    fn new() -> Self {
+        Self {
+            tasks: VecDeque::new(),
+        }
+    }
+}
+
+#[cfg(not(target_family = "wasm"))]
+impl VfsExtension for AsyncTestVFS {
+    const NAME: &'static str = "async_testvfs";
+    type File = AsyncTestFile;
+
+    fn open_file(&self, _path: &str, _flags: i32, _direct: bool) -> ExtResult<Self::File> {
+        let _ = env_logger::try_init();
+        log::info!("Opening new file: async test vfs");
+        Ok(AsyncTestFile {
+            storage: Arc::new(Mutex::new(BTreeMap::new())),
+            size: Arc::new(AtomicUsize::new(0)),
+        })
+    }
+
+    fn run_once(&self) -> ExtResult<()> {
+        log::info!("run_once async test VFS");
+        let mut task_queue = TASK_QUEUE.lock().unwrap();
+        while let Some(task) = task_queue.tasks.pop_front() {
+            let _ = RUNTIME.block_on(task);
+        }
+        Ok(())
+    }
+}
+
+#[cfg(not(target_family = "wasm"))]
+impl VfsFile for AsyncTestFile {
+    fn read(&mut self, mut buf: BufferRef, offset: i64, cb: Box<dyn FnOnce(i32) + Send>) {
+        let storage = Arc::clone(&self.storage);
+        let offset = offset as usize;
+
+        let task = RUNTIME.spawn(async move {
+            log::debug!(
+                "reading file with async testing VFS: {} offset: {offset}",
+                buf.len()
+            );
+            tokio::time::sleep(Duration::from_millis(20)).await;
+            let mut storage = storage.lock().unwrap();
+            let read_len = buf.len();
+            {
+                let read_buf = buf.as_mut_slice();
+                let mut pos = offset;
+                let mut remaining = read_len;
+                let mut buf_offset = 0;
+
+                while remaining > 0 {
+                    let page_no = pos / PAGE_SIZE;
+                    let page_offset = pos % PAGE_SIZE;
+                    let bytes_to_read = remaining.min(PAGE_SIZE - page_offset);
+                    let data = storage.entry(page_no).or_insert(Box::new([0; PAGE_SIZE]));
+                    read_buf[buf_offset..buf_offset + bytes_to_read]
+                        .copy_from_slice(&data[page_offset..page_offset + bytes_to_read]);
+                    pos += bytes_to_read;
+                    buf_offset += bytes_to_read;
+                    remaining -= bytes_to_read;
+                }
+            }
+            cb(read_len as i32);
+        });
+
+        TASK_QUEUE.lock().unwrap().tasks.push_back(task);
+    }
+
+    fn write(&mut self, buf: BufferRef, offset: i64, cb: Box<dyn FnOnce(i32) + Send>) {
+        let storage = Arc::clone(&self.storage);
+        let offset = offset as usize;
+        let size = self.size.clone();
+        let task = RUNTIME.spawn(async move {
+            log::debug!(
+                "writing to file with async testing VFS: {} offset: {offset}",
+                buf.len()
+            );
+            tokio::time::sleep(Duration::from_millis(20)).await;
+            let mut storage = storage.lock().unwrap();
+            let mut pos = offset;
+            let mut remaining = buf.len();
+            let mut buf_offset = 0;
+            let data = &buf.as_slice();
+            while remaining > 0 {
+                let page_no = pos / PAGE_SIZE;
+                let page_offset = pos % PAGE_SIZE;
+                let bytes_to_write = remaining.min(PAGE_SIZE - page_offset);
+                {
+                    let page = storage.entry(page_no).or_insert(Box::new([0; PAGE_SIZE]));
+                    page[page_offset..page_offset + bytes_to_write]
+                        .copy_from_slice(&data[buf_offset..buf_offset + bytes_to_write]);
+                }
+                pos += bytes_to_write;
+                buf_offset += bytes_to_write;
+                remaining -= bytes_to_write;
+            }
+            size.store(
+                core::cmp::max(
+                    offset + buf.len(),
+                    size.load(std::sync::atomic::Ordering::Relaxed),
+                ),
+                std::sync::atomic::Ordering::Relaxed,
+            );
+
+            cb(buf.len() as i32);
+        });
+
+        TASK_QUEUE.lock().unwrap().tasks.push_back(task);
+    }
+
+    fn sync(&self, cb: Box<dyn FnOnce() + Send>) {
+        let task = RUNTIME.spawn(async move {
+            tokio::time::sleep(Duration::from_millis(5)).await;
+            cb();
+        });
+        TASK_QUEUE.lock().unwrap().tasks.push_back(task);
+    }
+
+    fn size(&self) -> i64 {
+        self.size.load(std::sync::atomic::Ordering::Relaxed) as i64
     }
 }
