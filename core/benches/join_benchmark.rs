00666b730418900ae7110511abe7adecdbf27c28
Aydan Pirani <aydanpirani@gmail.com>
2025-04-23 15:10:17 -0500

    Start to benchmarking

--- core/benches/join_benchmark.rs
+++ core/benches/join_benchmark.rs
@@ -1,7 +1,38 @@
 use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
 use limbo_core::{Database, PlatformIO, IO};
+
+
 use pprof::criterion::{Output, PProfProfiler};
 use std::sync::Arc;
+
+pub struct TempDatabase {
+    _dir: TempDir,
+    path: PathBuf,
+}
+
+impl TempDatabase {
+    pub fn new_empty() -> Self {
+        let dir = TempDir::new().expect("failed to create tempdir");
+        let path = dir.path().join("database.db");
+        File::create(&path).expect("failed to create temp db file");
+        TempDatabase { _dir: dir, path }
+    }
+
+    pub fn connect_limbo(&self) -> limbo_core::Connection {
+        let io: Arc<dyn IO> = Arc::new(PlatformIO::new().unwrap());
+        let db = Database::open_file(io, &self.path, false)
+            .expect("opening limbo database failed");
+        db.connect().expect("limbo connect failed")
+    }
+
+    pub fn connect_rusqlite(&self) -> rusqlite::Connection {
+        let conn = rusqlite::Connection::open(&self.path).unwrap();
+        conn.pragma_update(None, "locking_mode", "EXCLUSIVE").unwrap();
+        conn
+    }
+}
+
+
 
 fn rusqlite_open() -> rusqlite::Connection {
     let sqlite_conn = rusqlite::Connection::open("../testing/database.db").unwrap();
@@ -20,13 +51,30 @@ fn bench_join_query(criterion: &mut Criterion) {
     let io = Arc::new(PlatformIO::new().unwrap());
     let db = Database::open_file(io.clone(), "../testing/database.db", false).unwrap();
     let limbo_conn = db.connect().unwrap();
+    
+    let tmp_db = TempDatabase::new_empty();
+    let conn = tmp_db.connect_limbo();
+
+    let create_table_query = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY)";
+    limbo_conn.execute(create_table_query).unwrap();
+
+    for i in 0..100000 {
+        let insert_query = format!("INSERT INTO users VALUES ({})", i);
+        limbo_conn.execute(insert_query).unwrap();
+    }
+
+
+    // let queries = [
+    //     "SELECT u.first_name, u.last_name, p.name AS product_name, o.quantity, o.order_date \
+    //      FROM users u \
+    //      JOIN orders o ON u.id = o.user_id \
+    //      JOIN products p ON o.product_id = p.id \
+    //      LIMIT 10",
+    // ];
 
     let queries = [
-        "SELECT u.first_name, u.last_name, p.name AS product_name, o.quantity, o.order_date \
-         FROM users u \
-         JOIN orders o ON u.id = o.user_id \
-         JOIN products p ON o.product_id = p.id \
-         LIMIT 10",
+        "SELECT u.id \
+         FROM users u"
     ];
 
     for query in queries.iter() {
