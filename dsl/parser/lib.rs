97025763fd479c414f4f6f4d92568f8e694631de
pedrocarlo <pedro.muniz.carlo@gmail.com>
2025-05-11 15:41:00 -0300

    use chumsky to create basic parser without expected value

--- dsl/parser/lib.rs
+++ dsl/parser/lib.rs
@@ -1,2 +1,72 @@
+use std::borrow::Cow;
+
+use chumsky::prelude::*;
+use rusqlite::types::Value;
+
+#[derive(Debug)]
+struct Test<'a> {
+    /// List of databases to test on
+    databases: Option<Vec<&'a str>>,
+    /// Test Name
+    ident: Cow<'a, str>,
+    /// Sql Statement(s) to run
+    statement: Statement<'a>,
+    /// Expected Output Value from test
+    values: Vec<Vec<Value>>,
+}
+
+/// Sql Statement(s)
+#[derive(Debug)]
+enum Statement<'a> {
+    Single(&'a str),
+    Many(Vec<&'a str>),
+}
+
+fn parser<'src>() -> impl Parser<'src, &'src str, Test<'src>, extra::Err<Rich<'src, char>>> {
+    let test_keyword = text::keyword("test");
+
+    let ident: Boxed<'_, '_, &'src str, &'src str, extra::Full<Rich<'src, char>, (), ()>> =
+        text::unicode::ident().padded().boxed();
+
+    let sql_query = text::unicode::ident();
+
+    let statement = choice((
+        sql_query.map(|s: &'src str| Statement::Single(s)),
+        sql_query
+            .separated_by(just(',').padded())
+            .allow_trailing()
+            .collect::<Vec<_>>()
+            .delimited_by(just('[').padded(), just(']').padded())
+            .map(|s: Vec<&str>| Statement::Many(s)),
+    ))
+    .boxed();
+
+    let contents = ident
+        .then_ignore(just(',').padded())
+        .then(statement)
+        .map(|(ident, statement)| Test {
+            databases: None,
+            ident: ident.into(),
+            statement,
+            values: vec![],
+        })
+        .delimited_by(just('(').padded(), just(')').padded())
+        .boxed();
+
+    test_keyword.ignore_then(contents).boxed()
+}
+
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use chumsky::Parser;
+
+    use crate::parser;
+
+    #[test]
+    fn test_basic() {
+        let parser = parser();
+        let x = parser.parse("test(hi, [SELECT ])");
+        dbg!(&x);
+        x.unwrap();
+    }
+}
