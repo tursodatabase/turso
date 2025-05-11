dfec0a0e3334c925eebecd302fb4907e9715b1ce
pedrocarlo <pedro.muniz.carlo@gmail.com>
2025-05-11 19:16:30 -0300

    parse integer values

--- dsl/parser/sqlite_values.rs
+++ dsl/parser/sqlite_values.rs
@@ -1,6 +1,16 @@
 use chumsky::prelude::*;
 use rusqlite::types::Value;
 
+pub(super) fn integer<'src>() -> impl Parser<'src, &'src str, i64, extra::Err<Rich<'src, char>>> {
+    let number = just('-')
+        .or_not()
+        .then(text::int(10))
+        .to_slice()
+        .map(|s: &str| s.parse().unwrap())
+        .boxed();
+    number
+}
+
 pub(super) fn value_parser<'src>(
 ) -> impl Parser<'src, &'src str, Value, extra::Err<Rich<'src, char>>> {
     let boolean = choice((
@@ -8,7 +18,10 @@ pub(super) fn value_parser<'src>(
         just("false").to(Value::Integer(0)),
     ))
     .boxed();
-    boolean
+
+    // let real = text::digits(radix);
+
+    choice((boolean, integer().map(|i| Value::Integer(i))))
 }
 
 pub(super) fn sqlite_values_parser<'src>(
@@ -46,6 +59,30 @@ mod tests {
         assert_debug_snapshot_with_input!(input, val);
 
         let input = "false";
+        let val = parser.parse(input).unwrap();
+        assert_debug_snapshot_with_input!(input, val);
+    }
+
+    #[test]
+    fn test_integer_value() {
+        let parser = value_parser();
+        let input = "1";
+        let val = parser.parse(input).unwrap();
+        assert_debug_snapshot_with_input!(input, val);
+
+        let input = "0";
+        let val = parser.parse(input).unwrap();
+        assert_debug_snapshot_with_input!(input, val);
+
+        let input = "-1";
+        let val = parser.parse(input).unwrap();
+        assert_debug_snapshot_with_input!(input, val);
+
+        let input = "-1000021900";
+        let val = parser.parse(input).unwrap();
+        assert_debug_snapshot_with_input!(input, val);
+
+        let input = "112343543009010293";
         let val = parser.parse(input).unwrap();
         assert_debug_snapshot_with_input!(input, val);
     }
