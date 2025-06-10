32e90f1e5cd1d2c88fb6a2f59f5e46dbd7cb7194
Diego Reis <reisdiego144@gmail.com>
2025-06-10 11:29:47 -0300

    core: Add experimental `close_async` to Connection

--- core/lib.rs
+++ core/lib.rs
@@ -50,6 +50,8 @@ pub use io::{
 use limbo_sqlite3_parser::{ast, ast::Cmd, lexer::sql::Parser};
 use parking_lot::RwLock;
 use schema::Schema;
+use std::future::{self, Future};
+use std::task::Poll;
 use std::{
     borrow::Cow,
     cell::{Cell, RefCell, UnsafeCell},
@@ -577,6 +579,17 @@ impl Connection {
                 }
             };
         }
+    }
+
+    pub fn close_async(&self) -> impl Future<Output = Result<()>> + '_ {
+        future::poll_fn(move |cx| match self.pager.checkpoint() {
+            Ok(CheckpointStatus::Done(_)) => Poll::Ready(Ok(())),
+            Ok(CheckpointStatus::IO) => {
+                cx.waker().wake_by_ref();
+                Poll::Pending
+            }
+            Err(e) => Poll::Ready(Err(e)),
+        })
     }
 
     pub fn last_insert_rowid(&self) -> i64 {
