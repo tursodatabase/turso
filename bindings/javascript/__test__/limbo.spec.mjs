1cc3e76af0ce86b5c6ad5ef2aeab80af327e62ef
Forato <lucas.e.forato@gmail.com>
2025-06-22 12:08:51 -0300

    feat: include tests in limbo wrapper as well

--- bindings/javascript/__test__/limbo.spec.mjs
+++ bindings/javascript/__test__/limbo.spec.mjs
@@ -13,13 +13,23 @@ test("Open in-memory database", async (t) => {
 test("Property .name of in-memory database", async (t) => {
   let name = ":memory:";
   const db = new Database(name);
-  t.is(db.name,name);
+  t.is(db.name, name);
 });
 
 test("Property .name of database", async (t) => {
   let name = "foobar.db";
   const db = new Database(name);
-  t.is(db.name,name);
+  t.is(db.name, name);
+});
+
+test("Property .readonly of database if set", async (t) => {
+  const db = new Database("foobar.db", { readonly: true });
+  t.is(db.readonly, true);
+});
+
+test("Property .readonly of database if not set", async (t) => {
+  const db = new Database("foobar.db");
+  t.is(db.readonly, false);
 });
 
 test("Statement.get() returns data", async (t) => {
@@ -126,7 +136,6 @@ test("Test raw(): Rows should be returned as arrays", async (t) => {
   db.prepare("INSERT INTO users (name, age) VALUES (?, ?)").run("Alice", 42);
   db.prepare("INSERT INTO users (name, age) VALUES (?, ?)").run("Bob", 24);
 
-
   let stmt = db.prepare("SELECT * FROM users").raw();
 
   for (const row of stmt.iterate()) {
@@ -158,7 +167,6 @@ test("Presentation modes should be mutually exclusive", async (t) => {
   db.prepare("CREATE TABLE users (name TEXT, age INTEGER)").run();
   db.prepare("INSERT INTO users (name, age) VALUES (?, ?)").run("Alice", 42);
   db.prepare("INSERT INTO users (name, age) VALUES (?, ?)").run("Bob", 24);
-
 
   // test raw()
   let stmt = db.prepare("SELECT * FROM users").pluck().raw();
@@ -200,7 +208,10 @@ test("Test exec(): Should correctly load multiple statements from file", async (
   const __dirname = path.dirname(__filename);
 
   const [db] = await connect(":memory:");
-  const file = fs.readFileSync(path.resolve(__dirname, "./artifacts/basic-test.sql"), "utf8");
+  const file = fs.readFileSync(
+    path.resolve(__dirname, "./artifacts/basic-test.sql"),
+    "utf8",
+  );
   db.exec(file);
   let rows = db.prepare("SELECT * FROM users").iterate();
   for (const row of rows) {
@@ -212,7 +223,7 @@ test("Test exec(): Should correctly load multiple statements from file", async (
 test("pragma query", async (t) => {
   const [db] = await connect(":memory:");
   let page_size = db.pragma("page_size");
-  let expectedValue = [{page_size: 4096}];
+  let expectedValue = [{ page_size: 4096 }];
   t.deepEqual(page_size, expectedValue);
 });
 
@@ -221,22 +232,36 @@ test("pragma table_list", async (t) => {
   let param = "sqlite_schema";
   let actual = db.pragma(`table_info(${param})`);
   let expectedValue = [
-    {cid: 0, name: "type", type: "TEXT", notnull: 0, dflt_value: null, pk: 0},
-    {cid: 1, name: "name", type: "TEXT", notnull: 0, dflt_value: null, pk: 0},
-    {cid: 2, name: "tbl_name", type: "TEXT", notnull: 0, dflt_value: null, pk: 0},
-    {cid: 3, name: "rootpage", type: "INT", notnull: 0, dflt_value: null, pk: 0},
-    {cid: 4, name: "sql", type: "TEXT", notnull: 0, dflt_value: null, pk: 0},
+    { cid: 0, name: "type", type: "TEXT", notnull: 0, dflt_value: null, pk: 0 },
+    { cid: 1, name: "name", type: "TEXT", notnull: 0, dflt_value: null, pk: 0 },
+    {
+      cid: 2,
+      name: "tbl_name",
+      type: "TEXT",
+      notnull: 0,
+      dflt_value: null,
+      pk: 0,
+    },
+    {
+      cid: 3,
+      name: "rootpage",
+      type: "INT",
+      notnull: 0,
+      dflt_value: null,
+      pk: 0,
+    },
+    { cid: 4, name: "sql", type: "TEXT", notnull: 0, dflt_value: null, pk: 0 },
   ];
   t.deepEqual(actual, expectedValue);
 });
 
-test("Test Statement.database gets the database object", async t => {
+test("Test Statement.database gets the database object", async (t) => {
   const [db] = await connect(":memory:");
   let stmt = db.prepare("SELECT 1");
   t.is(stmt.database, db);
 });
 
-test("Test Statement.source", async t => {
+test("Test Statement.source", async (t) => {
   const [db] = await connect(":memory:");
   let sql = "CREATE TABLE t (id int)";
   let stmt = db.prepare(sql);
@@ -246,7 +271,7 @@ test("Test Statement.source", async t => {
 test("simple pragma table_list", async (t) => {
   const [db] = await connect(":memory:");
   let param = "sqlite_schema";
-  let actual = db.pragma(`table_info(${param})`, {simple: true});
+  let actual = db.pragma(`table_info(${param})`, { simple: true });
   let expectedValue = 0;
   t.deepEqual(actual, expectedValue);
 });
--- bindings/javascript/wrapper.js
+++ bindings/javascript/wrapper.js
@@ -13,13 +13,18 @@ class Database {
    * @param {string} path - Path to the database file.
    * @param {Object} opts - Options for database behavior.
    * @param {boolean} [opts.readonly=false] - Open the database in read-only mode.
-   * @param {boolean} [opts.file_must_exist=false] - If true, throws if database file does not exist.
-   * @param {function} [opts.verbose] - Optional logging callback for SQL execution.
+   * @param {boolean} [opts.fileMustExist=false] - If true, throws if database file does not exist.
+   * @param {number} [opts.timeout=0] - Timeout duration in milliseconds for database operations. Defaults to 0 (no timeout).
    */
-  constructor(path, opts) {
+  constructor(path, opts = {}) {
+    opts.readonly = opts.readonly === undefined ? false : opts.readonly;
+    opts.fileMustExist = opts.fileMustExist === undefined ? false : opts.fileMustExist;
+    opts.timeout = opts.timeout === undefined ? 0 : opts.timeout;
+
     this.db = new NativeDB(path, opts);
     this.memory = this.db.memory;
     const db = this.db;
+
     Object.defineProperties(this, {
       inTransaction: {
         get() {
