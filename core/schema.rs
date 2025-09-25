3170077952316835028833782045724ce0a1e9e4
Jussi Saurio <jussi.saurio@gmail.com>
2025-09-25 16:19:28 +0300

    Fix incorrect test

--- core/schema.rs
+++ core/schema.rs
@@ -1999,13 +1999,12 @@ mod tests {
     }
 
     #[test]
-    pub fn test_column_is_rowid_alias_inline_composite_primary_key() -> Result<()> {
+    pub fn test_multiple_pk_forbidden() -> Result<()> {
         let sql = r#"CREATE TABLE t1 (a INTEGER PRIMARY KEY, b TEXT PRIMARY KEY);"#;
-        let table = BTreeTable::from_sql(sql, 0)?;
-        let column = table.get_column("a").unwrap().1;
+        let table = BTreeTable::from_sql(sql, 0);
+        let error = table.unwrap_err();
         assert!(
-            !table.column_is_rowid_alias(column),
-            "column 'a´ shouldn't be a rowid alias because table has composite primary key"
+            matches!(error, LimboError::ParseError(e) if e.contains("table \"t1\" has more than one primary key"))
         );
         Ok(())
     }
