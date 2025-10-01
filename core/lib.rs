70b1dd37e4926d78bd7a9b295fdda434d2be628f
pedrocarlo <pedro.muniz.carlo@gmail.com>
2025-10-01 13:02:09 -0300

    pass optional waker to step

--- core/lib.rs
+++ core/lib.rs
@@ -62,6 +62,7 @@ pub use io::{
 };
 use parking_lot::RwLock;
 use schema::Schema;
+use std::task::Waker;
 use std::{
     borrow::Cow,
     cell::RefCell,
@@ -2426,7 +2427,7 @@ impl BusyTimeout {
             }
         }
 
-        self.iteration += 1;
+        self.iteration = self.iteration.saturating_add(1);
         self.timeout = now + delay;
     }
 }
@@ -2492,7 +2493,7 @@ impl Statement {
         self.state.interrupt();
     }
 
-    pub fn step(&mut self) -> Result<StepResult> {
+    fn _step(&mut self, waker: Option<&Waker>) -> Result<StepResult> {
         if let Some(busy_timeout) = self.busy_timeout.as_ref() {
             if self.pager.io.now() < busy_timeout.timeout {
                 // Yield the query as the timeout has not been reached yet
@@ -2506,6 +2507,7 @@ impl Statement {
                 self.mv_store.as_ref(),
                 self.pager.clone(),
                 self.query_mode,
+                waker,
             )
         } else {
             const MAX_SCHEMA_RETRY: usize = 50;
@@ -2514,6 +2516,7 @@ impl Statement {
                 self.mv_store.as_ref(),
                 self.pager.clone(),
                 self.query_mode,
+                waker,
             );
             for attempt in 0..MAX_SCHEMA_RETRY {
                 // Only reprepare if we still need to update schema
@@ -2527,6 +2530,7 @@ impl Statement {
                     self.mv_store.as_ref(),
                     self.pager.clone(),
                     self.query_mode,
+                    waker,
                 );
             }
             res
@@ -2562,6 +2566,14 @@ impl Statement {
         }
 
         res
+    }
+
+    pub fn step(&mut self) -> Result<StepResult> {
+        self._step(None)
+    }
+
+    pub fn step_with_waker(&mut self, waker: &Waker) -> Result<StepResult> {
+        self._step(Some(waker))
     }
 
     pub(crate) fn run_ignore_rows(&mut self) -> Result<()> {
