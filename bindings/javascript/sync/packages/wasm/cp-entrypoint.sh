cff23c80cba6b8716fe0ade39e4f1bb55017f0ee
Nikita Sivukhin <sivukhin@turso.tech>
2025-10-02 16:56:54 +0400

    fix wasm client

---
+++ bindings/javascript/sync/packages/wasm/cp-entrypoint.sh
@@ -0,0 +1,3 @@
+sed 's/index-default.js/index-bundle.js/g' promise-default.ts > promise-bundle.ts
+sed 's/index-default.js/index-turbopack-hack.js/g' promise-default.ts > promise-turbopack-hack.ts
+sed 's/index-default.js/index-vite-dev-hack.js/g' promise-default.ts > promise-vite-dev-hack.ts
