926fd25cdd76a4fc5cfe600639787c882436fe8b
Nikita Sivukhin <sivukhin@turso.tech>
2025-10-06 18:01:54 +0400

    move examples to the top-level directory

--- bindings/javascript/examples/drizzle/example.js
+++
@@ -1,8 +0,0 @@
-import { drizzle } from "drizzle-orm/better-sqlite3";
-import { sql } from "drizzle-orm";
-import { Database } from '@tursodatabase/database';
-
-const sqlite = new Database('sqlite.db');
-const db = drizzle({ client: sqlite });
-const result = await db.all(sql`select 1`);
-console.log(result);
