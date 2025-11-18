4c32745bab9fbb8b2fe2506b508d091734d27bbb
Tejasmadhukar <tejasmadhukar6@gmail.com>
2025-11-18 19:34:06 +0530

    Limit Like(X,Y) case folding to ASCII Disable case folding for non-ASCII characters so they remain case-sensitive.

--- core/vdbe/value.rs
+++ core/vdbe/value.rs
@@ -1026,10 +1026,17 @@ pub fn construct_like_regex(pattern: &str) -> Regex {
             '%' => regex_pattern.push_str(".*"),
             '_' => regex_pattern.push('.'),
             ch => {
-                if regex_syntax::is_meta_character(c) {
-                    regex_pattern.push('\\');
+                if ch.is_ascii_alphabetic() {
+                    regex_pattern.push('[');
+                    regex_pattern.push(ch.to_ascii_lowercase());
+                    regex_pattern.push(ch.to_ascii_uppercase());
+                    regex_pattern.push(']');
+                } else {
+                    if regex_syntax::is_meta_character(c) {
+                        regex_pattern.push('\\');
+                    }
+                    regex_pattern.push(ch);
                 }
-                regex_pattern.push(ch);
             }
         }
     }
@@ -1037,7 +1044,6 @@ pub fn construct_like_regex(pattern: &str) -> Regex {
     regex_pattern.push('$');
 
     RegexBuilder::new(&regex_pattern)
-        .case_insensitive(true)
         .dot_matches_new_line(true)
         .build()
         .unwrap()
