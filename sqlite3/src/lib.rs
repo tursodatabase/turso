f5e8eaa6c14ec6fd05d12f7532f67482b698624a
Nikita Sivukhin <sivukhin@turso.tech>
2025-11-21 13:57:34 +0400

    enable indices by default

--- sqlite3/src/lib.rs
+++ sqlite3/src/lib.rs
@@ -184,7 +184,7 @@ pub unsafe extern "C" fn sqlite3_open(
             Err(_) => return SQLITE_CANTOPEN,
         },
     };
-    match turso_core::Database::open_file(io.clone(), filename_str, false, false) {
+    match turso_core::Database::open_file(io.clone(), filename_str, false, true) {
         Ok(db) => {
             let conn = db.connect().unwrap();
             let filename = match filename_str {
