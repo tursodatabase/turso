fd2b39f45c624c485d1ffcc2f69d641c0e7b2db2
Nikita Sivukhin <sivukhin@turso.tech>
2025-12-03 16:46:44 +0400

    generate sync bindings

--- bindings/go/bindings.go
+++ bindings/go/bindings.go
@@ -3,9 +3,10 @@ package turso
 import "fmt"
 
 func init() {
-	library, err := loadLibrary("turso_sdk_kit")
+	library, err := loadLibrary("turso_sync_sdk_kit")
 	if err != nil {
 		panic(fmt.Errorf("unable to load turso library: %w", err))
 	}
 	register_turso_db(library)
+	register_turso_sync(library)
 }
