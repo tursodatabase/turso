cb988699b294cd9881b59f9cdc812a47dd7b874e
pedrocarlo <pedro.muniz.carlo@gmail.com>
2025-12-13 14:22:41 -0300

    stub backend code + write comparison functions

--- turso-test-runner/src/lib.rs
+++ turso-test-runner/src/lib.rs
@@ -1,4 +1,8 @@
+pub mod backends;
+pub mod comparison;
 pub mod parser;
 
+pub use backends::{BackendError, DatabaseInstance, QueryResult, SqlBackend};
+pub use comparison::{compare, ComparisonResult};
 pub use parser::ast::{DatabaseConfig, DatabaseLocation, Expectation, TestCase, TestFile};
 pub use parser::{parse, ParseError};
