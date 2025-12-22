ca9deedafe47a43aeec895f8b009bf0b59e4aa4e
Pavan-Nambi <pavannambi999@gmail.com>
2025-12-22 19:41:39 +0530

    use workspace

--- bindings/ruby/ext/turso/extconf.rb
+++ bindings/ruby/ext/turso/extconf.rb
@@ -4,5 +4,6 @@ require "mkmf"
 require "rb_sys/mkmf"
 
 create_rust_makefile("turso/turso") do |r|
+  r.features = ["extension-module"]
   r.extra_rustflags = ["--cfg", "rb_sys_gem"]
 end
--- bindings/ruby/ext/turso/src/lib.rs
+++ bindings/ruby/ext/turso/src/lib.rs
@@ -1,12 +1,20 @@
+#[cfg(feature = "extension-module")]
 use magnus::{Error, Ruby};
 
+#[cfg(feature = "extension-module")]
 mod connection;
+#[cfg(feature = "extension-module")]
 mod database;
+#[cfg(feature = "extension-module")]
 mod errors;
+#[cfg(feature = "extension-module")]
 mod statement;
+#[cfg(feature = "extension-module")]
 mod value;
+#[cfg(feature = "extension-module")]
 mod setup;
 
+#[cfg(feature = "extension-module")]
 #[magnus::init(name = "turso")]
 fn init(ruby: &Ruby) -> Result<(), Error> {
     let module = ruby.define_module("Turso")?;
