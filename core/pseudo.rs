6388e0c898625e87bb243a21f522fd7c2ac05ae8
pedrocarlo <pedro.muniz.carlo@gmail.com>
2025-12-23 13:37:55 -0300

    remove unnecessary refcel from `PseudoCursor`

--- core/pseudo.rs
+++ core/pseudo.rs
@@ -1,5 +1,3 @@
-use std::cell::{Ref, RefCell};
-
 use crate::{
     types::{ImmutableRecord, RecordCursor},
     Result, Value,
@@ -7,30 +5,34 @@ use crate::{
 
 pub struct PseudoCursor {
     record_cursor: RecordCursor,
-    current: RefCell<Option<ImmutableRecord>>,
+    current: Option<ImmutableRecord>,
 }
 
 impl Default for PseudoCursor {
+    #[inline]
     fn default() -> Self {
         Self {
             record_cursor: RecordCursor::new(),
-            current: RefCell::new(None),
+            current: None,
         }
     }
 }
 
 impl PseudoCursor {
-    pub fn record(&self) -> Ref<Option<ImmutableRecord>> {
-        self.current.borrow()
+    #[inline]
+    pub fn record(&self) -> Option<&ImmutableRecord> {
+        self.current.as_ref()
     }
 
+    #[inline]
     pub fn insert(&mut self, record: ImmutableRecord) {
         self.record_cursor.invalidate();
-        self.current.replace(Some(record));
+        self.current = Some(record);
     }
 
+    #[inline]
     pub fn get_value(&mut self, column: usize) -> Result<Value> {
-        if let Some(record) = self.current.borrow().as_ref() {
+        if let Some(record) = self.current.as_ref() {
             Ok(self.record_cursor.get_value(record, column)?.to_owned())
         } else {
             Ok(Value::Null)
