00b0890c8fef7f1ca58ed24da2c1a872816e2c1a
Nikita Sivukhin <sivukhin@turso.tech>
2025-12-26 21:38:55 +0400

    do not log error in case of not found file for open
    - this can be valid situation in sync engine

--- core/io/generic.rs
+++ core/io/generic.rs
@@ -13,7 +13,7 @@ impl GenericIO {
 }
 
 impl IO for GenericIO {
-    #[instrument(err, skip_all, level = Level::TRACE)]
+    #[instrument(skip_all, level = Level::TRACE)]
     fn open_file(&self, path: &str, flags: OpenFlags, direct: bool) -> Result<Arc<dyn File>> {
         trace!("open_file(path = {})", path);
         let mut file = std::fs::File::options();
