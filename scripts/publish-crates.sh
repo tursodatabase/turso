a21b8b27476d626eb5c1ca43d87e0b41136af295
Pekka Enberg <penberg@iki.fi>
2026-01-05 11:56:52 +0200

    Fix Rust crate publishing even more

--- scripts/publish-crates.sh
+++ scripts/publish-crates.sh
@@ -6,4 +6,6 @@ cargo publish -p turso_parser
 cargo publish -p turso_core
 cargo publish -p turso_sdk_kit_macros
 cargo publish -p turso_sdk_kit
+cargo publish -p turso_sync_engine
+cargo publish -p turso_sync_sdk_kit
 cargo publish -p turso
