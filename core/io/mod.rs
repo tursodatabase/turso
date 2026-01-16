a1aba085626ca6dc94ddd265689621ef1c7f2507
Jussi Saurio <jussi.saurio@gmail.com>
2026-01-16 17:39:06 +0200

    Merge 'do not call unlock_after_restart in case of error during wal truncation - because we already released these locks earlier' from Nikita Sivukhin
    We properly unlock read locks during checkpoint before log truncation
    ```rust
    CheckpointState::Finalize { .. } => {
      if mode.should_restart_log() {
        Self::unlock_after_restart(&self.shared, None);
      }
      ...
    }
    ```
    So, there is no need to do that in any case for any operation after that
    phase (and we truncate log outside of main checkpoint state machine)
    
    Reviewed-by: Jussi Saurio <jussi.saurio@gmail.com>
    
    Closes #4691
    
    (cherry picked from commit 25ce2b21c804f2edbfb493badd9ebf7a65793fe4)

--- core/io/mod.rs
+++ core/io/mod.rs
@@ -11,7 +11,7 @@ use std::sync::Arc;
 use std::{fmt::Debug, pin::Pin};
 
 cfg_block! {
-   #[cfg(all(target_os = "linux", feature = "io_uring", not(miri)))] {
+    #[cfg(all(target_os = "linux", feature = "io_uring", not(miri)))] {
         mod io_uring;
         #[cfg(feature = "fs")]
         pub use io_uring::UringIO;
