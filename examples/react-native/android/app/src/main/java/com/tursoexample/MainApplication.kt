73674bf420d0fa5734c31963c2ef6eaf85e1fe39
Nikita Sivukhin <sivukhin@turso.tech>
2026-01-18 15:24:58 +0400

    wip

--- examples/react-native/android/app/src/main/java/com/tursoexample/MainApplication.kt
+++ examples/react-native/android/app/src/main/java/com/tursoexample/MainApplication.kt
@@ -3,11 +3,8 @@ package com.tursoexample
 import android.app.Application
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
-import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
-import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
 
@@ -22,18 +19,12 @@ class MainApplication : Application(), ReactApplication {
 
             override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
 
-            override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-            override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+            override val isNewArchEnabled: Boolean = false
+            override val isHermesEnabled: Boolean = false
         }
-
-    override val reactHost: ReactHost
-        get() = getDefaultReactHost(applicationContext, reactNativeHost)
 
     override fun onCreate() {
         super.onCreate()
         SoLoader.init(this, false)
-        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-            load()
-        }
     }
 }
