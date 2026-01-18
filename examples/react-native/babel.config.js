fbc362edf0e60669240e58ea0b166b6d9b2a8b4f
Nikita Sivukhin <sivukhin@turso.tech>
2026-01-18 16:45:58 +0400

    wip

--- examples/react-native/babel.config.js
+++ examples/react-native/babel.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const { getConfig } = require('react-native-builder-bob/babel-config');
 const pkg = require('../../bindings/react-native/package.json');
 
-const root = path.resolve(__dirname, '..');
+const root = path.resolve(__dirname, '../../bindings/react-native');
 
 module.exports = getConfig(
   {
