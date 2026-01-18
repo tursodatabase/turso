bb41ddd8fefb235fa1f4cb5d62af333375a53689
Nikita Sivukhin <sivukhin@turso.tech>
2026-01-18 16:41:23 +0400

    wip

--- examples/react-native/babel.config.js
+++ examples/react-native/babel.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
-const {getConfig} = require('react-native-builder-bob/babel-config');
-const pkg = require('../package.json');
+const { getConfig } = require('react-native-builder-bob/babel-config');
+const pkg = require('../../bindings/react-native/package.json');
 
 const root = path.resolve(__dirname, '..');
 
@@ -8,5 +8,5 @@ module.exports = getConfig(
   {
     presets: ['module:@react-native/babel-preset'],
   },
-  {root, pkg},
+  { root, pkg },
 );
