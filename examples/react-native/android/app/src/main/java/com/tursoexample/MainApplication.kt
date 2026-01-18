7316555d86eb47b62d4a07d424ad5f9287c8da21
Nikita Sivukhin <sivukhin@turso.tech>
2026-01-18 15:19:47 +0400

    wip

--- examples/react-native/android/app/src/main/java/com/tursoexample/MainApplication.kt
+++ examples/react-native/android/app/src/main/java/com/tursoexample/MainApplication.kt
@@ -10,16 +10,13 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
-import com.turso.reactnative.TursoPackage
 
 class MainApplication : Application(), ReactApplication {
 
     override val reactNativeHost: ReactNativeHost =
         object : DefaultReactNativeHost(this) {
             override fun getPackages(): List<ReactPackage> =
-                PackageList(this).packages.apply {
-                    add(TursoPackage())
-                }
+                PackageList(this).packages
 
             override fun getJSMainModuleName(): String = "index"
 
