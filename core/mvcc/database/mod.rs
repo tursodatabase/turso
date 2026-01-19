b7ab2aa55ad7af83402a797ad9b253dab18d9a72
Pere Diaz Bou <pere-altea@hotmail.com>
2026-01-19 17:43:16 +0100

    core/mvcc: repase_schema after stream of schema changes
    When creating a table with many UNIQUE constraints that force automatic
    indices, it will translated to a stream of logical log records. If we
    reparse for every new automatic index creation record we will end up in
    a panic because table is still not loaded up with all indices so we
    needt o wait to add all of them to reparse.

--- core/mvcc/database/mod.rs
+++ core/mvcc/database/mod.rs
@@ -2967,8 +2967,20 @@ impl<Clock: LogicalClock> MvStore<Clock> {
                 Ok(index_info)
             }
         };
+        let mut last_was_schema_insert = false;
         loop {
             let next_rec = reader.next_record(&pager.io, &mut get_index_info)?;
+
+            // Check if we need to reparse schema before processing this operation
+            // This happens when transitioning from schema inserts to any other operation
+            let needs_reparse = last_was_schema_insert
+                && !matches!(&next_rec, StreamingResult::InsertTableRow { rowid, .. } if rowid.table_id == SQLITE_SCHEMA_MVCC_TABLE_ID);
+            if needs_reparse {
+                connection.reparse_schema()?;
+                *connection.db.schema.lock() = connection.schema.read().clone();
+                last_was_schema_insert = false;
+            }
+
             match next_rec {
                 StreamingResult::InsertTableRow { row, rowid } => {
                     let is_schema_row = rowid.table_id == SQLITE_SCHEMA_MVCC_TABLE_ID;
@@ -3012,13 +3024,7 @@ impl<Clock: LogicalClock> MvStore<Clock> {
                         assert!(self.table_id_to_rootpage.get(&rowid.table_id).is_some(), "Logical log contains a row version insert with a table id {} that does not exist in the table_id_to_rootpage map: {:?}", rowid.table_id, self.table_id_to_rootpage.iter().collect::<Vec<_>>());
                     }
                     self.insert(tx_id, row)?;
-                    if is_schema_row {
-                        // Make sure the newly parsed schema change record gets populated into the in-memory schema object.
-                        // It's a bit inefficient this way (we could just deserialize the logical log row as well), but schema
-                        // changes in the logical log are special cases that don't happen that often.
-                        connection.reparse_schema()?;
-                        *connection.db.schema.lock() = connection.schema.read().clone();
-                    }
+                    last_was_schema_insert = is_schema_row;
                 }
                 StreamingResult::DeleteTableRow { row, rowid } => {
                     assert!(self.table_id_to_rootpage.get(&rowid.table_id).is_some(), "Logical log contains a row version delete with a table id that does not exist in the table_id_to_rootpage map: {}", rowid.table_id);
@@ -3038,6 +3044,13 @@ impl<Clock: LogicalClock> MvStore<Clock> {
                 }
             }
         }
+
+        // Do a final reparse if the last operation was a schema insert
+        if last_was_schema_insert {
+            connection.reparse_schema()?;
+            *connection.db.schema.lock() = connection.schema.read().clone();
+        }
+
         self.commit_load_tx(tx_id, &connection);
         Ok(true)
     }
