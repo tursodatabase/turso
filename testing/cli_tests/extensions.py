9b67b54395fd6be2fce073b99c387674c5ac4410
gigagrug <55370306+gigagrug@users.noreply.github.com>
2026-01-24 14:21:37 -0500

    support regexp() before and after ext loads

--- testing/cli_tests/extensions.py
+++ testing/cli_tests/extensions.py
@@ -73,11 +73,9 @@ def null(res):
 def test_regexp():
     turso = TestTursoShell(test_data)
     extension_path = "./target/debug/liblimbo_regexp"
-    # before extension loads, assert no function
-    turso.run_test_fn(
-        "SELECT regexp('a.c', 'abc');",
-        lambda res: "Parse error: no such function" in res,
-    )
+    # before extension loads
+    turso.run_test_fn("SELECT regexp('a.c', 'abc');", true)
+    # after extension loads
     turso.run_test_fn(f".load {extension_path}", null)
     console.info(f"Extension {extension_path} loaded successfully.")
     turso.run_test_fn("SELECT regexp('a.c', 'abc');", true)
