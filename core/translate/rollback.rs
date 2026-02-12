a9dcbd94a929da487696c1ada60a3c8d93c3a8d1
PThorpe92 <preston@turso.tech>
2026-02-12 11:18:42 -0500

    Translate/emit SAVEPOINT and RELEASE ast nodes

--- core/translate/rollback.rs
+++ core/translate/rollback.rs
@@ -1,24 +1,44 @@
-use turso_parser::ast::Name;
-
 use crate::{
-    bail_parse_error,
-    vdbe::{builder::ProgramBuilder, insn::Insn},
+    vdbe::{
+        builder::ProgramBuilder,
+        insn::{Insn, SavepointOp},
+    },
     Result,
 };
+use turso_parser::ast::Name;
+
+pub fn translate_savepoint(mut program: ProgramBuilder, name: Name) -> Result<ProgramBuilder> {
+    program.emit_insn(Insn::Savepoint {
+        op: SavepointOp::Begin,
+        name: name.as_str().to_string(),
+    });
+    Ok(program)
+}
+
+pub fn translate_release(mut program: ProgramBuilder, name: Name) -> Result<ProgramBuilder> {
+    program.emit_insn(Insn::Savepoint {
+        op: SavepointOp::Release,
+        name: name.as_str().to_string(),
+    });
+    Ok(program)
+}
 
 pub fn translate_rollback(
-    program: &mut ProgramBuilder,
-    txn_name: Option<Name>,
+    mut program: ProgramBuilder,
+    _txn_name: Option<Name>,
     savepoint_name: Option<Name>,
-) -> Result<()> {
-    if txn_name.is_some() || savepoint_name.is_some() {
-        bail_parse_error!("txn_name and savepoint not supported yet");
+) -> Result<ProgramBuilder> {
+    if let Some(savepoint_name) = savepoint_name {
+        program.emit_insn(Insn::Savepoint {
+            op: SavepointOp::RollbackTo,
+            name: savepoint_name.as_str().to_string(),
+        });
+    } else {
+        program.emit_insn(Insn::AutoCommit {
+            auto_commit: true,
+            rollback: true,
+        });
+        program.rollback();
     }
-
-    program.emit_insn(Insn::AutoCommit {
-        auto_commit: true,
-        rollback: true,
-    });
-    program.rollback();
-    Ok(())
+    Ok(program)
 }
