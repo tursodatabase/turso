9788bcf7ffc64e878e805ffbaaa4aab5bb815dc6
Pekka Enberg <penberg@iki.fi>
2026-02-12 11:12:03 +0200

    core/mvcc: Don't spam the logs

--- core/mvcc/database/mod.rs
+++ core/mvcc/database/mod.rs
@@ -1117,7 +1117,7 @@ impl<Clock: LogicalClock> StateTransition for CommitStateMachine<Clock> {
                     }
                     Err(err) => {
                         // Auto-checkpoint errors should not surface to the committed statement.
-                        tracing::info!("MVCC auto-checkpoint failed: {err}");
+                        tracing::debug!("MVCC auto-checkpoint failed: {err}");
                         self.finalize(mvcc_store)?;
                         return Ok(TransitionResult::Done(()));
                     }
