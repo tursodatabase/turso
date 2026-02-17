db3d39b1f1e5f808f1cec0cfb90f80188866d312
Nikita Sivukhin <sivukhin@turso.tech>
2026-02-17 17:44:33 +0100

    export type from serverless driver

--- packages/turso-serverless/src/index.ts
+++ packages/turso-serverless/src/index.ts
@@ -1,5 +1,6 @@
 // Turso serverless driver entry point
 export { Connection, connect, type Config } from './connection.js';
 export { Statement } from './statement.js';
+export { Session, type SessionConfig } from './session.js';
 export { DatabaseError } from './error.js';
 export { type Column, ENCRYPTION_KEY_HEADER } from './protocol.js';
