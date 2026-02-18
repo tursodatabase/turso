0927537554edc1a15f6f7f75877d224a70c254d9
osamasalem <osama__salem@hotmail.com>
2026-02-18 04:38:45 +0200

    feature gate for win_iocp

--- core/io/mod.rs
+++ core/io/mod.rs
@@ -27,13 +27,15 @@ cfg_block! {
         pub use PlatformIO as SyscallIO;
     }
 
-    #[cfg(all(target_os = "windows", not(miri)))] {
+    #[cfg(all(target_os = "windows", feature = "win_iocp", not(miri)))] {
         mod win_iocp;
         #[cfg(feature = "fs")]
         pub use win_iocp::WindowsIOCP;
+        pub use win_iocp::WindowsIOCP as PlatformIO;
+        pub use PlatformIO as SyscallIO;
     }
 
-    #[cfg(any(not(any(target_family = "unix", target_os = "android", target_os = "ios")), miri))] {
+    #[cfg(any(not(any(all(target_os = "windows", feature = "win_iocp"), target_family = "unix", target_os = "android", target_os = "ios")), miri))] {
         mod generic;
         pub use generic::GenericIO as PlatformIO;
         pub use PlatformIO as SyscallIO;
