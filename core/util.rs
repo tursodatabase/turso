3ddd1e424feac8e1681d0790e8913d07cf09b892
Eren Turkoglu <erenxturkoglu@gmail.com>
2026-03-05 21:13:39 +0100

    Fix case-insensitive behavior using ASCII-only lowercase

--- core/util.rs
+++ core/util.rs
@@ -123,7 +123,7 @@ const QUOTE_PAIRS: &[(char, char)] = &[
 pub fn normalize_ident(identifier: &str) -> String {
     // quotes normalization already happened in the parser layer (see Name ast node implementation)
     // so, we only need to convert identifier string to lowercase
-    identifier.to_lowercase()
+    identifier.to_ascii_lowercase()
 }
 
 pub const PRIMARY_KEY_AUTOMATIC_INDEX_NAME_PREFIX: &str = "sqlite_autoindex_";
