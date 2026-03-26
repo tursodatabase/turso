299fc08df4ee04e85ab83249d7b2ec1e6a2c7815
pedrocarlo <pedro.muniz.carlo@gmail.com>
2026-03-26 17:32:57 -0300

    make OnSerializationComplete Callback return a `Result<()>`

--- tests/integration/mvcc.rs
+++ tests/integration/mvcc.rs
@@ -46,7 +46,7 @@ impl turso_core::mvcc::persistent_storage::DurableStorage for RecordingDurableSt
     fn log_tx(
         &self,
         m: &turso_core::mvcc::database::LogRecord,
-        on_serialization_complete: Option<&dyn Fn(&[u8], u32)>,
+        on_serialization_complete: Option<&dyn Fn(&[u8], u32) -> turso_core::Result<()>>,
     ) -> turso_core::Result<(turso_core::Completion, u64)> {
         self.used_log_tx
             .store(true, std::sync::atomic::Ordering::SeqCst);
