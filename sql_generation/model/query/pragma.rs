b77155dc49c64accbef76f221a39a5deb516126e
Pavan Nambi <pavannambi999@gmail.com>
2026-05-05 06:23:02 +0530

    sql-generation: model storage pragmas

--- sql_generation/model/query/pragma.rs
+++ sql_generation/model/query/pragma.rs
@@ -6,6 +6,10 @@ use serde::{Deserialize, Serialize};
 pub enum Pragma {
     AutoVacuumMode(VacuumMode),
     ForeignKeyList(String),
+    IntegrityCheck,
+    PageSize,
+    PageSizeSet(u32),
+    WalCheckpoint(WalCheckpointMode),
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -13,6 +17,14 @@ pub enum VacuumMode {
     None,
     Incremental,
     Full,
+}
+
+#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
+pub enum WalCheckpointMode {
+    Passive,
+    Full,
+    Restart,
+    Truncate,
 }
 
 impl Display for Pragma {
@@ -31,6 +43,18 @@ impl Display for Pragma {
             Pragma::ForeignKeyList(table_name) => {
                 let table_name = table_name.replace('\'', "''");
                 write!(f, "PRAGMA foreign_key_list('{table_name}')")
+            }
+            Pragma::IntegrityCheck => write!(f, "PRAGMA integrity_check"),
+            Pragma::PageSize => write!(f, "PRAGMA page_size"),
+            Pragma::PageSizeSet(page_size) => write!(f, "PRAGMA page_size={page_size}"),
+            Pragma::WalCheckpoint(mode) => {
+                let mode = match mode {
+                    WalCheckpointMode::Passive => "PASSIVE",
+                    WalCheckpointMode::Full => "FULL",
+                    WalCheckpointMode::Restart => "RESTART",
+                    WalCheckpointMode::Truncate => "TRUNCATE",
+                };
+                write!(f, "PRAGMA wal_checkpoint({mode})")
             }
         }
     }
