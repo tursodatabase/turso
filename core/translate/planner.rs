9486e043e975f4f5444f7304ecc1a6cc52ff4118
howenyap <wenhyap02@gmail.com>
2026-06-04 14:37:29 +0800

    do not split between statement if lhs is a subquery and update snapshot

--- core/translate/planner.rs
+++ core/translate/planner.rs
@@ -2253,6 +2253,10 @@ pub fn rewrite_between_exprs(expr: &mut Expr) -> Result<()> {
             end,
         } = e
         {
+            if matches!(lhs.as_ref(), Expr::Subquery(_)) {
+                return Ok(WalkControl::Continue);
+            }
+
             let lhs_expr = std::mem::take(lhs.as_mut());
             let start_expr = std::mem::take(start.as_mut());
             let end_expr = std::mem::take(end.as_mut());
