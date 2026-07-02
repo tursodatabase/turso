d1659ebfe1e9e6e9d0b702b6b4b7a0d8e2db7d24
Mikaël Francoeur <mikael@turso.tech>
2026-07-02 14:50:13 -0400

    randomize params and run integrity check as eventually

--- testing/antithesis/stress-io_uring-mvcc/singleton_driver_stress.sh
+++ testing/antithesis/stress-io_uring-mvcc/singleton_driver_stress.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-/bin/turso_stress --nr-threads 2 --nr-iterations 10000 --vfs io_uring --tx-mode concurrent
+/bin/turso_stress --nr-threads $((1 + RANDOM % 5)) --nr-iterations $((1000 + RANDOM % 9000)) --vfs io_uring --tx-mode concurrent --db-file /tmp/stress.db
--- testing/antithesis/stress-unreliable/singleton_driver_stress.sh
+++ testing/antithesis/stress-unreliable/singleton_driver_stress.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-LD_PRELOAD=/usr/lib/unreliable-libc.so /bin/turso_stress --nr-iterations 10000
+LD_PRELOAD=/usr/lib/unreliable-libc.so /bin/turso_stress --nr-threads $((1 + RANDOM % 5)) --nr-iterations $((1000 + RANDOM % 9000)) --db-file /tmp/stress.db
